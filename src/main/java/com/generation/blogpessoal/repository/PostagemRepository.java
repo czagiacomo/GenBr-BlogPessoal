6b8080eaf9fc5de4b84037fa208603b566e01d73
Catharina Zagiacomo <catharina.zagiacomo@gmail.com>
2024-11-29 15:36:35 -0300

    Tema added

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -10,7 +10,5 @@ import com.generation.blogpessoal.model.Postagem;
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 	
 	public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
-	
-	
-	
+		
 }
